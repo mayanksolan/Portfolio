0069fc15628eeccff4b112b1488d38fb1dbc8ad5
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-06-09 03:01:42 +0530

    7th

--- app.js
+++ app.js
@@ -6,3 +6,8 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res) {
 	res.render("index");
 });
+
+var urlPort = process.env.PORT || "3000";
+app.listen(urlPort, process.env.IP, function() {
+	console.log("Portfolio server has started");
+});
