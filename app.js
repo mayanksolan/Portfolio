1e133c26b3568cd3b3c34975d09ef8708b6c1f44
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-06-10 01:24:37 +0530

    11th

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ var express = require("express");
 var app = express();
 
 app.set("view engine", "ejs");
-app.set('views', './app/views');
 
 app.get("/", function(req, res) {
-	res.render("index");
+	res.render("views/index");
 });
 
 var urlPort = process.env.PORT || "3000";
