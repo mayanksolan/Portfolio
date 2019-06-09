5e71c87e93f04259dd3d9ea1212eb97a2ab7d878
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-06-10 01:21:05 +0530

    10th

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 
 app.set("view engine", "ejs");
+app.set('views', './app/views');
 
 app.get("/", function(req, res) {
 	res.render("index");
