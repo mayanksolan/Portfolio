8725f6116a780a9792a01b4a621a974a0cc93a7f
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-06-10 01:42:42 +0530

    15th

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require("express");
 var app = express();
 
 app.set("view engine", "ejs");
-app.use(express.static(__dirname+"../public"));
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function(req, res) {
 	res.render("index");
